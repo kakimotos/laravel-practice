b9c6d3da272360484856ed1ccf4e11eaf627f2cd
kakimotos <ikaruga.hajme@gmail.com>
2023-11-25 23:53:34 +0900

    todoリストインデックスページを作る

---
+++ src/laraveltokyo/app/Http/Controllers/TaskController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index(Request $request)
+    {
+        // $tasks = $request->only['name','detail'];
+        // Task::create($tasks);
+        return view('todo.index');
+    }
+}
---
+++ src/laraveltokyo/resources/views/todo/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="">
+    {{-- <form method="GET" action="{{ route('index.send') }}">
+
+    </form> --}}
+    <p>task一覧を表示したい</p>
+</div>
+@endsection
